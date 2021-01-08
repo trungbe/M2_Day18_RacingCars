1bc2a4ee611367a63525d10f11512689ac067890
trungbe <trungnd0601@gmail.com>
2021-01-08 10:46:44 +0700

    racing car

---
+++ src/Car.java
@@ -0,0 +1,44 @@
+import java.util.Random;
+
+public class Car implements Runnable {
+
+    private String name;
+
+    public Car(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public void run() {
+        int runDistance = 0;
+        long startTime = System.currentTimeMillis();
+        while (runDistance < Main.DISTANCE ) {
+            try {
+                // Random Speed KM/H
+                int speed = (new Random()).nextInt(20);
+                // Calculate traveled distance
+                runDistance += speed;
+                // Build result graphic
+                String log = "|";
+                int percentTravel = (runDistance * 100) / Main.DISTANCE;
+                for (int i = 0; i < Main.DISTANCE; i += Main.STEP) {
+                    if (percentTravel >= i + Main.STEP) {
+                        log += "=";
+                    } else if (percentTravel >= i && percentTravel < i + Main.STEP) {
+                        log += "o";
+                    } else {
+                        log += "-";
+                    }
+                }
+                log += "|";
+                System.out.println("Car" + this.name + ": " + log + " " + Math.min(Main.DISTANCE, runDistance) + "KM");
+                Thread.sleep(1100);
+            } catch (InterruptedException e) {
+                System.out.println("Car" + this.name + " broken...");
+                break;
+            }
+        }
+        long endTime = System.currentTimeMillis();
+        System.out.println("Car" + this.name + " Finish in " + (endTime - startTime) / 1000 + "s");
+    }
+}
